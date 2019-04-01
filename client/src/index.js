0f10d5bcc4a3f7be0e162a1828766ffd70170642
wanzargen <wanjin911003@gmail.com>
2019-04-01 17:50:08 +0900

    created project

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker'; //브라우저가 백그라운드에서 실행하는 스크립트. 웹페이지와는 별개로 작동. 푸시 알림 및 백그라운드 동기화와 같은 기능 제공.
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

596965b8d9d48c7b00dcfe26373798dce39450d9
wanzargen <wanjin911003@gmail.com>
2019-04-02 18:47:52 +0900

    Added study note

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'; //React is a open-source JS library. to develop UI (User Interface) on web application or site. Its fully component base.
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  }
+
   render() {
     return (
       <div className="Home">
-        HOME!!!
+        <input></input>
       </div>
     );
   }

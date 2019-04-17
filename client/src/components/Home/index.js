0ee2a6a69e592eed175eec51843082a3242577dd
wanzargen <wanjin911003@gmail.com>
2019-04-17 12:43:04 +0900

    -

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'; //React is a open-source JS library. to develop UI (User Interface) on web application or site. Its fully component base.
 
+const table = <table>
+                <thead>
+                  <th>Date</th>
+                  <th>Comment</th>
+                  <th>Amount</th>
+                  <th>Unit</th>
+                  <th>For</th>
+                  <th>Payed</th>
+                </thead>
+                <tbody>
+                </tbody>
+              </table>
+
 class Home extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      list: []
+    };
   }
 
   render() {
     return (
       <div className="Home">
-        <input></input>
+        {table}
       </div>
     );
   }

ac8f4a203ed460fc9e3c3673f1ea1afdfd4060d7
wanzargen <wanjin911003@gmail.com>
2019-04-20 13:07:36 +0900

    added topbar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,5 @@
 import React, { Component } from 'react'; //React is a open-source JS library. to develop UI (User Interface) on web application or site. Its fully component base.
 
-const table = <table>
-                <thead>
-                  <th>Date</th>
-                  <th>Comment</th>
-                  <th>Amount</th>
-                  <th>Unit</th>
-                  <th>For</th>
-                  <th>Payed</th>
-                </thead>
-                <tbody>
-                </tbody>
-              </table>
-
 class Home extends Component {
 
   constructor(props) {
@@ -25,7 +12,7 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        {table}
+        Home 내용 ----
       </div>
     );
   }
---
+++ src/components/TopBar/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'; 
+
+class TopBar extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        TopBar!
+      </div>
+    );
+  }
+}
+
+export default TopBar;
